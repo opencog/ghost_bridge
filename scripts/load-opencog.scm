1a38f0c316b1657a62e2aab01ada68046f4149d9
Ralf Mayet <trilly@gmail.com>
2018-05-25 11:46:32 +0800

    Update load-opencog.scm

--- scripts/load-opencog.scm
+++ scripts/load-opencog.scm
@@ -10,6 +10,7 @@
              (opencog ghost procedures)
              (opencog exec))
 
+
 (load "load-actions.scm")
 
 (ecan-based-ghost-rules #t)
@@ -21,6 +22,34 @@
 ; load and start the ghost-bridge-action-node
 (use-modules (opencog ghost-bridge-action-node))
 (start-ghost-bridge-action-node)
+
+(define SPREADING_FILTER (ConceptNode "SPREADING_FILTER"))
+
+(define-public (ecan-set-spreading-filter . type-symbols)
+"
+  ecan-set-spreading-filter TYPE-SYMBOLS
+  Set ecan to filter atoms of TYPE-SYMOBLS.
+"
+  (if (not (null? type-symbols))
+    (StateLink
+      SPREADING_FILTER
+      (MemberLink
+        (map (lambda (x) (TypeNode (symbol->string x))) type-symbols))))
+
+  SPREADING_FILTER
+)
+
+(define (ghost-set-ecan-filter . add-types)
+  (apply ecan-set-spreading-filter
+    (filter (lambda (x) (not (member x add-types))) (cog-get-types)))
+)
+
+(define (ec)
+  (ghost-set-ecan-filter
+    'ImplicationLink 'WordNode 'ConceptNode 'PredicateNode 'EvaluationLink)
+)
+
+(ec)
 
 ; Start the ghost loop
 (ghost-run)
