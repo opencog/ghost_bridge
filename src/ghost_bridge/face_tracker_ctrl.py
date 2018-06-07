8522d3cf5410f398fadaca2351dcbb9906c30f08
Jamie Diprose <jamie.diprose@gmail.com>
2018-06-07 16:07:55 +0800

    Update face tracker code

--- src/ghost_bridge/face_tracker_ctrl.py
+++ src/ghost_bridge/face_tracker_ctrl.py
@@ -1,18 +1,22 @@
 import tf
 import rospy
+import math
 from blender_api_msgs.msg import Target
 
 
 class FaceTracker(object):
 
+    DIST_THRESH = 0.0
+
     def __init__(self):
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(2)
         self.tf = tf.TransformListener()
-        self.eye_speed = 0.7
-        self.head_speed = 0.5
+        self.eye_speed = 0.5
+        self.head_speed = 0.7
 
         self.face_frame = "closest_face"
         self.blender_frame = "blender"
+        self.last_position = None
 
         # Publishers for making the face and eyes look at a point
         self.face_target_pub = rospy.Publisher("/blender_api/set_face_target", Target, queue_size=1)
@@ -26,10 +30,23 @@ class FaceTracker(object):
                 x = position[0]
                 y = position[1]
                 z = position[2]
-                self.point_eyes_at_point(x, y, z, self.eye_speed)
-                self.face_toward_point(x, y, z, self.head_speed)
+
+                update_target = self.last_position is None
+
+                if self.last_position is not None:
+                    dist = FaceTracker.distance(position, self.last_position)
+                    update_target = dist > FaceTracker.DIST_THRESH
+
+                if update_target:
+                    self.point_eyes_at_point(x, y, z, self.eye_speed)
+                    self.face_toward_point(x, y, z, self.head_speed)
+                    self.last_position = position
 
             self.rate.sleep()
+
+    @staticmethod
+    def distance(p1, p2):
+        return math.sqrt(math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2) + math.pow(p1[2] - p2[2], 2))
 
     def point_eyes_at_point(self, x, y, z, speed):
         """  Turn the robot's eyes towards the given target point
