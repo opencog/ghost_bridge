8ae9f9d6e4dc5b1d8337ed4f6d48b0f56aaef8b6
Jamie Diprose <jamie.diprose@gmail.com>
2018-06-14 02:34:15 -0700

    hackedy hack but works nice

--- src/ghost_bridge/ghost_bridge_ctrl.py
+++ src/ghost_bridge/ghost_bridge_ctrl.py
@@ -12,7 +12,8 @@ from hr_msgs.msg import ChatMessage
 from hr_msgs.msg import TTS
 from ros_people_model.msg import Faces
 from std_msgs.msg import String
-
+from blender_api_msgs.msg import SetGesture
+import numpy as np
 
 class GhostBridge:
     EMOTION_MAP = {
@@ -42,6 +43,7 @@ class GhostBridge:
         self.tts_speaking = False
         self.sr_continuous = True
         self.sr_tts_timeout = 0.0
+        self.refractory_block = False
 
         self.timer = None
         self.input_buffer = ""
@@ -51,7 +53,7 @@ class GhostBridge:
         self.cs_fallback_queue = Queue(maxsize=1)
 
         self.tts_pub = rospy.Publisher(self.robot_name + "/tts", TTS, queue_size=1)
-
+        self.gesture_pub = rospy.Publisher("/blender_api/set_gesture", SetGesture, queue_size=1)
         rospy.Subscriber('/ghost_bridge/say', GhostSay, self.ghost_say_cb)
         rospy.Subscriber(self.robot_name + "/chatbot_responses", TTS, self.cs_say_cb)
         rospy.Subscriber(self.robot_name + "/speech_events", String, self.tts_say_cb)
@@ -60,6 +62,30 @@ class GhostBridge:
         rospy.Subscriber('/faces_throttled', Faces, self.faces_cb)
 
         self.dynamic_reconfigure_srv = Server(GhostBridgeConfig, self.dynamic_reconfigure_callback)
+
+
+    def gesture(self, name, speed, magnitude, repeat):
+        """ Set a pose on the robot's face
+
+        :param str name: the id of the gesture
+        :param float speed:
+        :param float magnitude: the magnitude of the gesture from 0.0 to 1.0
+        :param int32 repeat: number of times to repeat
+        :return: None
+        """
+
+        msg = SetGesture()
+        msg.name = name
+        msg.speed = speed
+        msg.magnitude = magnitude
+        msg.repeat = repeat  
+
+        self.gesture_pub.publish(msg)
+        rospy.logdebug("published gesture(name={}, speed={}, magnitude={}, repeat={})".format(name, speed, magnitude,
+                                                                                              repeat))
+
+
+
 
     def dynamic_reconfigure_callback(self, config, level):
         self.sr_continuous = config['sr_continuous']
@@ -86,7 +112,7 @@ class GhostBridge:
 
     def ghost_say_cb(self, msg):
         rospy.logdebug("ghost_say_cb: '{}', '{}'".format(msg.text, msg.fallback_id))
-
+        self.refractory_block = False
         if msg.fallback_id == "chatscript":
             try:
                 # wait for three seconds if no new response to give ChatScript a chance.
@@ -95,6 +121,7 @@ class GhostBridge:
                 cs_fallback_text = ""
                 rospy.logwarn("cs_fallback_text is ''")
             self.publish_tts(cs_fallback_text)
+            self.gesture("think-l",1.0,0.8,1)
         else:
             self.publish_tts(msg.text)
 
@@ -116,23 +143,36 @@ class GhostBridge:
     def perceive_word_cb(self, msg):
         self.perception_ctrl.perceive_word(self.face_id, msg.utterance)
         self.perception_ctrl.perceive_face_talking(self.face_id, 1.0)
+        if np.random.randint(4) == 3:
+            self.gesture("nod-1",0.9,np.random.random()*0.6,1)
         self.reset_timer()
 
     def perceive_sentence_cb(self, msg):
         #print("perceive sentence cb")
         if self.sr_continuous or not self.tts_speaking:
-            self.input_buffer += msg.utterance
+            # This should be append, but subverting it here because of ghost bug
+            self.input_buffer = msg.utterance
+            #self.input_buffer += msg.utterance
             self.reset_timer()
+            if self.stt_cutoff_time > 0.0:
+                self.gesture("nod-2",0.8,0.85,1)
         else:
             rospy.logwarn("suppressing sentence perceived to GHOST")
 
     def send_perceived_sentence(self, evt):
         #print("send perceived sentence called")
-        if self.input_buffer:
+        if self.input_buffer and not self.refractory_block:
           tosend = self.input_buffer
           self.input_buffer = ""
-          self.perception_ctrl.perceive_sentence(self.face_id, tosend)
+          #block perception for a while to fix blocking
+          self.refractory_block = True
+          #rospy.Timer(rospy.Duration((self.stt_cutoff_time * 2.0) + 0.0001), self.reset_refractory, oneshot=True)
+
+          self.perception_ctrl.perceive_sentence(self.face_id, tosend[:64])
           self.perception_ctrl.perceive_face_talking(self.face_id, 0.0)
+
+    def reset_refractory(self, evt):
+        self.refractory_block = False
 
     def reset_timer(self):
         #print("--> Reset timer called")
